c970729fdd7d33f310b298860d46e0693d1bb4be
devloverumar <softwareengineerumar@gmail.com>
2024-02-06 15:38:41 -0500

    minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -125,6 +125,6 @@ const IndexPage = () => (
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Home" />
+// export const Head = () => <Seo title="Home" />
 
 export default IndexPage
