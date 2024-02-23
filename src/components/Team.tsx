967313a956a3361f9cda4e9d78a35f3d340f89c1
devloverumar <softwareengineerumar@gmail.com>
2024-02-22 21:55:01 -0500

    profile links added

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -63,11 +63,11 @@ const StyledCardContainer = styled.div`
 `;
 
 
-const TeamMemberCard = ({ name, imageSrc, bio }) => {
+const TeamMemberCard = ({ name, imageSrc, bio, profileLink }) => {
 
     return (
         <StyledCard>
-            <CardActionArea>
+            <CardActionArea href={profileLink} target='_blank'>
                 <CardMedia>
                     <Avatar
                         alt="profile pic"
@@ -96,39 +96,44 @@ const Team = () => {
             name: 'Dr. Khalid Mahmood Malik',
             imageSrc: malikHeadshot,
             bio: 'Khalid Mahmood Malik is currently a Professor at Computer Science, University of Michigan-Flint. His research interests include multimedia forensics, development of intelligent decision support systems using analysis of medical imaging and clinical text, secure multicast protocols for intelligent transportation systems, and Automated ontology and knowledge graph generation. His research is supported by the National Science Foundation (NSF) and Brain Aneurysm Foundation.',
+            profileLink: "https://scholar.google.ca/citations?user=MZGQT2wAAAAJ&hl=en"
         },
         {
             name: 'Dr. Ijaz Ul Haq',
             imageSrc: ijazHeadshot,
             bio: 'Ijaz Ul Haq received the Ph.D. degree from the Sejong University, Seoul, Korea in 2022. He is currently working as Research Fellow in University of Michigan. His research interests include the multimodal and  neurosymbolic learning for multimedia forensics.',
+            profileLink: "https://scholar.google.ca/citations?hl=en&user=TdaSaRgAAAAJ"
         },
-
         {
             name: 'Awais Khan',
             imageSrc: awaisHeadshot,
             bio: 'Awais Khan is currently pursuing a Doctor of Philosophy in Computer Science at the School of Engineering and Computer Science, Oakland University, Rochester, Michigan, USA. His research interests revolve around the captivating realm of speech processing, with a particular emphasis on topics like Deepfake Detection, Automatic Speaker Verification, and Anti-Spoofing. He aims to enhance security and combat emerging threats within voice-based technologies.',
+            profileLink: "https://scholar.google.ca/citations?hl=en&user=N3awCpYAAAAJ"
         },
         {
             name: 'Muhammad Irfan',
             imageSrc: irfanHeadshot,
             bio: 'Muhammad Irfan is currently pursuing a Doctor of Philosophy in Computer Science at the School of Engineering and Computer Science, Oakland University, Rochester, Michigan, USA. His research interests revolve around the captivating realm of speech processing, with a particular emphasis on topics like Deepfake Detection, Automatic Speaker Verification, and Anti-Spoofing. He aims to enhance security and combat emerging threats within voice-based technologies.',
+            profileLink: "https://scholar.google.ca/citations?hl=en&user=MfhBA7EAAAAJ"
         },
         {
             name: 'James Ryan',
             imageSrc: jjHeadshot,
             bio: 'James Ryan is currently pursuing a Doctor of Philosophy in Computer Science at the School of Engineering and Computer Science, Oakland University, Rochester, Michigan, USA. His research interests revolve around the captivating realm of speech processing, with a particular emphasis on topics like Deepfake Detection, Automatic Speaker Verification, and Anti-Spoofing. He aims to enhance security and combat emerging threats within voice-based technologies.',
+            profileLink: "https://www.linkedin.com/in/jjryan111/"
         },
         {
             name: 'Muhammad Anas Raza',
             imageSrc: anasHeadshot,
             bio: 'Anas fell in love with programming in 6th grade when he learned that my friend could develop the games he played on his PC. He attempted to create a Visual Basic application in high school. He adores reading, writing, thinking about, improving, and understanding programs, especially in Python. At SMILES Lab, He works in using Computer Vision techniques for multimedia forensics personal website: https://anasrz.com',
+            profileLink: "https://anasrz.com"
         },
         {
             name: 'Muhammad Umar Farooq',
             imageSrc: umarHeadshot,
             bio: 'Muhammad Umar Farooq is currently pursuing a Doctor of Philosophy in Computer Science at the School of Engineering and Computer Science, Oakland University, Rochester, Michigan, USA. His research interests revolve around the captivating realm of speech processing, with a particular emphasis on topics like Deepfake Detection, Automatic Speaker Verification, and Anti-Spoofing. He aims to enhance security and combat emerging threats within voice-based technologies.',
+            profileLink: "https://www.linkedin.com/in/muhammad-umar-farooq-345a7a141/"
         },
-
     ];
 
     return (
@@ -139,6 +144,7 @@ const Team = () => {
                     name={member.name}
                     imageSrc={member.imageSrc}
                     bio={member.bio}
+                    profileLink={member.profileLink}
                 />
             ))}
         </StyledCardContainer>
